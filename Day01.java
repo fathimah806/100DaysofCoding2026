d434101ddb35cb890ef04fa7cca9ead761afe02b
fathimah806 <aszahra247@gmail.com>
2026-09-02 21:44:49 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,18 @@
+public class Main {
+     public static void main(String[] args) {
+
+        // Komentar satu baris
+        System.out.println("Struktur dasar kode Java adalah susunan utama program yang terdiri dari class, method main(), dan perintah program yang dijalankan oleh Java.");
+
+        /*
+         * Komentar lebih dari satu baris.
+         * Komentar ini digunakan untuk memberikan penjelasan yang terdiri dari beberapa baris 
+         * dan tidak akan dijalankan oleh Java.
+    
+         */
+
+        // Menampilkan nama
+        String nama = "Fatima Sahra";
+        System.out.println("Nama: " + nama);
+    }
+}
