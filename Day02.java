31289906b9b13b815d2a597a133bf61ba4c3530c
fathimah806 <aszahra247@gmail.com>
2026-09-03 08:03:37 +0800

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,24 @@
+public class Biodata {
+    public static void main(String[] args) {
+
+        // Menggunakan println()
+        System.out.println("=================================");
+        System.out.println("          BIODATA DIRI");
+        System.out.println("=================================");
+    
+
+        // Menggunakan print()
+        System.out.print("Nama       : ");
+        System.out.println("Fatima Sahra");
+
+        System.out.print("Alamat     : ");
+        System.out.println("Pasangkayu");
+
+        // Menggunakan printf()
+        System.out.printf("Umur       : %d tahun%n", 19);
+        System.out.printf("Jurusan    : %s%n", "Informatika");
+        System.out.printf("Fakultas   : %s%n", "Teknik");
+
+        System.out.println("=================================");
+    }
+}
