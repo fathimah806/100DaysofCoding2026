f836e149da0a030ff72557483004d9ffef78aa9c
fathimah806 <aszahra247@gmail.com>
2026-09-04 12:46:39 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,18 @@
+public class Day03 {
+    public static void main(String[] args) {
+        
+        // \n = pindah ke baris baru
+        System.out.println("Hallo\nDunia");
+
+        // \t = membuat jarak/tab
+        System.out.println("nama\tnilai");
+        System.out.println("Andi\t90");
+
+        // \" = menamppilkan ta da kutip
+        System.out.println("\"Belajar Java\"");
+
+        // \\ = menampilkan tanda backslash
+        System.out.println("c\\Java");
+    }
+    
+}
