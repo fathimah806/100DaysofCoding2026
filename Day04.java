6bebc5bec6688ab88fe266c734c7e6edf3f73457
fathimah806 <aszahra247@gmail.com>
2026-09-05 20:13:13 +0800

    Create Day04.java

---
+++ Day04.java
@@ -0,0 +1,15 @@
+public class Day04 {
+    public static  void main(String[] args) {
+
+        byte umur = 20;
+        short jumlahPenduduk = 15000;
+        int harga = 250000;
+        long jarak = 10000000000L;
+
+        System.out.println("Umur: " + umur);
+        System.out.println("Jumlah Penduduk: " + jumlahPenduduk);
+        System.out.println("Harga: " + harga);
+        System.out.println("Jarak: " + jarak);
+    }
+    
+}
