69ae8433472d37053fef2aba1159da2b7c64cd71
fathimah806 <aszahra247@gmail.com>
2026-09-06 20:52:24 +0800

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,13 @@
+public class Day05 {
+    public static void main(String[] args) {
+        
+        // Tipe data float
+        float tinggi = 170.5f;
+
+        // Tipe data double
+        double berat = 55.75;
+
+        System.out.println("Tinggi : " + tinggi + " cm" );
+        System.out.println("Berat : "+ berat + " kg");
+    }
+}
