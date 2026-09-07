83ffb43b327617799535ec7ef47515c6d1508f40
fathimah806 <aszahra247@gmail.com>
2026-09-07 20:03:50 +0800

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,16 @@
+public class day06 {
+    public static void main (String [] args) {
+
+        char jeniskelamin = 'L';
+        char nilai = 'A';
+
+        boolean mahasiswa = true;
+        boolean lulus = false;
+
+        System.out.println ("jenis kelamin : " + jeniskelamin);
+        System.out.println ("nilai : " + nilai);
+        System.out.println("mahasiswa : " + mahasiswa);
+        System.out.println("lulus : " + lulus);
+
+    }
+}
