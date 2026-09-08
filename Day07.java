ae9a1fce5414e8fa1d6cea96f948909629d42fc3
fathimah806 <aszahra247@gmail.com>
2026-09-08 22:08:10 +0800

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,22 @@
+public class Day07 {
+    public static void main(String[] args) {
+        
+        /* 
+        String adalah tipe data yang digunakan untuk menyimpan teks
+        dan menggunakantanda petik dua(" "). 
+        */ 
+        
+        // Contohnya
+
+        String nama = "Fatima sahra";
+        String alamat = "pasangkayu";
+        String prodi = "teknik Informatika";
+        
+        System.out.println("nama");
+        System.err.println("alamat");
+        System.out.println("prodi");
+
+
+    }
+    
+}
