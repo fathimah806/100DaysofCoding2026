36767be199487b3f40d486dc4b1a0c39d89d05ca
fathimah806 <aszahra247@gmail.com>
2026-09-09 20:44:26 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,19 @@
+public class Day08 {
+    public static void main(String[] args) {
+        
+        // Deklarasi Variabel
+        int nilai;
+
+        // Inisialisasi Variabel
+        nilai = 80;
+
+        System.out.println("Nilai awal: " + nilai );
+
+        // Update Variabel
+        nilai = 90;
+
+        System.out.println("Nilai setelah diubah : " + nilai );
+
+    }
+
+}
