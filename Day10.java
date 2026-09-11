7ea7884de061ef243ed64e3c4231706882dde4a2
fathimah806 <aszahra247@gmail.com>
2026-09-11 22:08:23 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,22 @@
+public class Day10 {
+    public static void main(String[] args) {
+
+        System.out.println("Batas Tipe Data Java");
+
+        System.out.println("byte");
+        System.out.println("Minimal : " + Byte. MIN_VALUE);
+        System.out.println("Maksimal : " + Byte.MAX_VALUE);
+
+        System.out.println("\nShort : "  );
+        System.out.println("Minimal : " + Short.MIN_VALUE );
+        System.out.println("Maksimal : " + Short.MAX_VALUE);
+
+        System.out.println("\nInteger : ");
+        System.out.println("Minimal : " + Integer.MIN_VALUE);
+        System.out.println("Maksimal:" + Integer.MAX_VALUE);
+
+        System.out.println("\nLong:");
+        System.out.println("Minimal:" + Long.MIN_VALUE);
+        System.out.println("maksimal:" + Long.MAX_VALUE);
+    }
+}
