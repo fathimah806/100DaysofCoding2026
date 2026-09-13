bf15aabce07dc344ddc22a921b1bf25a0cacba4a
fathimah806 <aszahra247@gmail.com>
2026-09-13 21:35:12 +0800

    Create Day 12.java

---
+++ Day 12.java
@@ -0,0 +1,15 @@
+public class latihan {
+    public static void main (String [] args)  {
+
+    String nama = "fatima";
+    int umur = 19;
+    char jk = 'P';
+    float tb = 1.73f;
+    boolean m = true;
+
+    System.out.printf(" nama : %s%n umur : %d%n ", nama,umur );
+    System.out.println("jenis kelamin : "+jk);
+    System.out.print(" tinggi badan : "+tb +" \n mahasiswa aktif : "+m);
+    
+    }
+}
