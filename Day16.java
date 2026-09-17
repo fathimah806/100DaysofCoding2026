496bd80f131604bfd09c4f9ca8a67f1aee6275fd
fathimah806 <aszahra247@gmail.com>
2026-09-17 21:25:04 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Day16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int sisa = a % b ;
+        System.out.println("hasil sisa :" + sisa);
+        
+       
+    }
+    
+}
