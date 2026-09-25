bb497c09f7c0ce1916ce6fdb7afb29008f0d3afc
fathimah806 <aszahra247@gmail.com>
2026-09-25 17:43:03 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day24 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int p = in.nextInt();
+        int L = in.nextInt();
+        int luas = p * L;
+
+        System.out.println("luas persegi panjang :" + luas);
+
+
+
+
+
+    }
+}
